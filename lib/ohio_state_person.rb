590a22ae595199f164493e8fb6494f5c6f5fc794
Barry Brinkley <brinkley.17@osu.edu>
2012-09-04 15:55:50 -0400

    Removed the stipulation that ID and EMPLID are the same.

--- lib/ohio_state_person.rb
+++ lib/ohio_state_person.rb
@@ -15,8 +15,6 @@ module OhioStatePerson
       validates_uniqueness_of :emplid
       validates_format_of :emplid, :with => /\A\d{8,9}\z/, :message => 'must be 8 or 9 digits'
 
-      before_validation :set_id, :on => :create
-      validate :id_is_emplid
     end
   end
 
@@ -51,15 +49,6 @@ module OhioStatePerson
     end
 
     protected
-    def set_id
-      self.id = self.emplid.to_i
-    end
-
-    def id_is_emplid
-      unless self.id == self.emplid.to_i
-        errors.add(:id, 'must be the same as the emplid')
-      end
-    end
   end
 
 end
